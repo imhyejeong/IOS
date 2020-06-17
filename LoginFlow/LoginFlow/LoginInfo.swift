f72673b01082eddfba856e848a08ea6ade343b0c
imhyejeong <65651736+imhyejeong@users.noreply.github.com>
2020-06-17 23:50:28 +0900

    Add files via upload

---
+++ LoginFlow/LoginFlow/LoginInfo.swift
@@ -0,0 +1,14 @@
+//
+//  LoginInfo.swift
+//  LoginFlow
+//
+//  Created by DS on 2020/06/17.
+//  Copyright © 2020 HJ. All rights reserved.
+//
+
+import Foundation
+
+struct LoginInfo {
+    let email: String
+    let password: String
+}
